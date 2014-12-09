e30bebb0fa886078b3bc8a9bb806df7f8887a7c8
Alexey Shapilov <alexey.shapilov@gmail.com>
2014-12-09 17:15:45 +0300

    Прижал footer к низу окна

--- content/static/footer.php
+++ content/static/footer.php
@@ -3,11 +3,13 @@ if (!defined('INIT')) exit('No direct script access allowed');
 
 ?>
 
-    <div class="b-footer clearfix">
-        <div class="split-line"></div>
-        <div class="b-footer_body">
-            <p class="b-footer_copyright">&copy; "Это мой сайт, пожалуйста, не копируйте и не воруйте его</p>
-        </div>
+<div class="footer-push"></div>
+</div>
+
+<div class="b-footer clearfix">
+    <div class="split-line"></div>
+    <div class="b-footer_body">
+        <p class="b-footer_copyright">&copy; "Это мой сайт, пожалуйста, не копируйте и не воруйте его</p>
     </div>
 </div>
 </body>
